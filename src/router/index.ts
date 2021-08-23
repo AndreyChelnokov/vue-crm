5c5b3cfe0bccb8b1fca60fbe4658a3f429a21f03
GATE31 <gate31@MacBook-Pro-web-razrabotcik.local>
2021-08-23 23:27:20 +0300

    create and start dev

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
+import Categories from '../views/Categories.vue';
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,16 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/categories',
+    name: 'Categories',
+    meta: {layout: 'main'},
+    component: Categories
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {layout: 'empty'},
+    component: Login
   }
 ]
 
